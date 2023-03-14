ecc14a9a85e089afcbfc242e3d9011f6ed27a72e
carolinalvarez <carolina.alvarezg21@gmail.com>
2023-03-14 18:06:44 +0100

    more simulations

--- R/simulation-study.R
+++ R/simulation-study.R
@@ -1290,4 +1290,548 @@ var_lcc
 write.csv(res, file = "~/Documents/Master/thesis/02-Thesis/code/code-MA-thesis/output/sim_c.csv", row.names = FALSE)
 
 
+# Simulation D
+
+set.seed(123)
+
+sim <- 300
+k <- 30
+N <- 100000
+r <- 0.9
+a <- 0.9
+ns_fixed1 <- 4000
+ns_fixed2 <- 2000
+
+mean1 <- c(rep(1, k/2), rep(0, k/2))
+mean0 <- c(rep(0, k))
+cov_mat <- diag(k)
+
+beta_names_cc <- paste0("β_hat_cc_", 0:k)
+beta_names_wcc <- paste0("β_hat_wcc_", 0:k)
+beta_names_lcc <- paste0("β_hat_lcc_", 0:k)
+
+
+output <- c(beta_names_cc, beta_names_wcc, beta_names_lcc)
+
+res <- data.frame(matrix(ncol = length(output), nrow = 0))
+colnames(res) <- output
+
+for (i in 1:sim) {
+  
+  df <- gdp.imbalanced(N = N, r = r, distribution= "gaussian", k = k, mean1 = mean1
+                       , mean0 = mean0, sigma1 = cov_mat, sigma0 = cov_mat)
+  
+  
+  cc_output <- cc_algorithm_fixed(data=df, r=r, a=a, ns_fixed=ns_fixed1)
+  coef_adjusted_cc <- cc_output$coef_adjusted
+  
+  wcc_output <- wcc_algorithm_fixed(data=df, r=r, a=a, ns_fixed = ns_fixed1)
+  coef_unadjusted_wcc <- wcc_output$coef_unadjusted
+  
+  lcc_output <- lcc_algorithm_fixed(data=df, r=r, a_wcc=a, ns_fixed = ns_fixed2)
+  coef_adjusted_lcc <- lcc_output$coef_adjusted
+  
+  # a_bar(lambda)
+  #a_bar_lcc <- mean(lcc_output$a_bar_lcc)
+  
+  # Predicting subsampling
+  # y_hat_cc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_cc)
+  # y_hat_wcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_unadjusted_wcc)
+  # y_hat_lcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_lcc)
+  
+  # Coefficients
+  res_cc <- data.frame(t(coef_adjusted_cc))
+  colnames(res_cc) <- beta_names_cc
+  
+  res_wcc <- data.frame(t(coef_unadjusted_wcc))
+  colnames(res_wcc) <- beta_names_wcc
+  
+  res_lcc <- data.frame(t(coef_adjusted_lcc))
+  colnames(res_lcc) <- beta_names_lcc
+  
+  #AUC
+  
+  # auc_cc <- as.numeric(roc(df_test$y, y_hat_cc)$auc)
+  # auc_wcc <- as.numeric(roc(df_test$y, y_hat_wcc)$auc)
+  # auc_lcc <- as.numeric(roc(df_test$y, y_hat_lcc)$auc)
+  
+  res <- rbind(res
+               , cbind(res_cc, res_wcc, res_lcc))
+  
+}
+
+# take the mean of the results
+means <- data.frame(t(colMeans(res)))
+colnames(means) <- gsub("β_hat_", "", colnames(means))
+
+
+# True coefficient values
+beta_true <- c(get.true.intercept(1-r, rep(0.5, k), c(rep(1,k/2), rep(0, k/2))), rep(1, k/2)
+               , rep(0, k/2))
+
+beta_true <- rep(beta_true, 3)
+
+# Calculate squared bias
+squared_bias <- (means - beta_true)^2
+
+# Add column names to squared_bias
+colnames(squared_bias) <- colnames(means)
+
+# Display squared_bias
+squared_bias_cc <- sum(squared_bias[1:as.numeric(k+1)])
+squared_bias_cc
+squared_bias_wcc <- sum(squared_bias[as.numeric(k+2):as.numeric(k+k+2)])
+squared_bias_wcc
+squared_bias_lcc <- sum(squared_bias[as.numeric(k+k+3):length(squared_bias)])
+squared_bias_lcc
+
+# Take the variance of the realizations
+variances <- apply(res, 2, var)
+
+var_cc <- sum(variances[1:as.numeric(k+1)])
+var_cc
+var_wcc <- sum(variances[as.numeric(k+2):as.numeric(k+k+2)])
+var_wcc
+var_lcc <- sum(variances[as.numeric(k+k+3):length(variances)])
+var_lcc
+
+write.csv(res, file = "~/Documents/Master/thesis/02-Thesis/code/code-MA-thesis/output/sim_d.csv", row.names = FALSE)
+
+# Simulation E
+
+set.seed(123)
+
+sim <- 500
+k <- 30
+N <- 100000
+r <- 0.9
+a <- 0.9
+ns_fixed1 <- 4000
+ns_fixed2 <- 2000
+
+mean1 <- c(rep(1, k/2), rep(0, k/2))
+mean0 <- c(rep(0, k))
+cov_mat <- diag(k)
+
+beta_names_cc <- paste0("β_hat_cc_", 0:k)
+beta_names_wcc <- paste0("β_hat_wcc_", 0:k)
+beta_names_lcc <- paste0("β_hat_lcc_", 0:k)
+
+
+output <- c(beta_names_cc, beta_names_wcc, beta_names_lcc)
+
+res <- data.frame(matrix(ncol = length(output), nrow = 0))
+colnames(res) <- output
+
+for (i in 1:sim) {
+  
+  df <- gdp.imbalanced(N = N, r = r, distribution= "gaussian", k = k, mean1 = mean1
+                       , mean0 = mean0, sigma1 = cov_mat, sigma0 = cov_mat)
+  
+  
+  cc_output <- cc_algorithm_fixed(data=df, r=r, a=a, ns_fixed=ns_fixed1)
+  coef_adjusted_cc <- cc_output$coef_adjusted
+  
+  wcc_output <- wcc_algorithm_fixed(data=df, r=r, a=a, ns_fixed = ns_fixed1)
+  coef_unadjusted_wcc <- wcc_output$coef_unadjusted
+  
+  lcc_output <- lcc_algorithm_fixed(data=df, r=r, a_wcc=a, ns_fixed = ns_fixed2)
+  coef_adjusted_lcc <- lcc_output$coef_adjusted
+  
+  # a_bar(lambda)
+  #a_bar_lcc <- mean(lcc_output$a_bar_lcc)
+  
+  # Predicting subsampling
+  # y_hat_cc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_cc)
+  # y_hat_wcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_unadjusted_wcc)
+  # y_hat_lcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_lcc)
+  
+  # Coefficients
+  res_cc <- data.frame(t(coef_adjusted_cc))
+  colnames(res_cc) <- beta_names_cc
+  
+  res_wcc <- data.frame(t(coef_unadjusted_wcc))
+  colnames(res_wcc) <- beta_names_wcc
+  
+  res_lcc <- data.frame(t(coef_adjusted_lcc))
+  colnames(res_lcc) <- beta_names_lcc
+  
+  #AUC
+  
+  # auc_cc <- as.numeric(roc(df_test$y, y_hat_cc)$auc)
+  # auc_wcc <- as.numeric(roc(df_test$y, y_hat_wcc)$auc)
+  # auc_lcc <- as.numeric(roc(df_test$y, y_hat_lcc)$auc)
+  
+  res <- rbind(res
+               , cbind(res_cc, res_wcc, res_lcc))
+  
+}
+
+# take the mean of the results
+means <- data.frame(t(colMeans(res)))
+colnames(means) <- gsub("β_hat_", "", colnames(means))
+
+
+# True coefficient values
+beta_true <- c(get.true.intercept(1-r, rep(0.5, k), c(rep(1,k/2), rep(0, k/2))), rep(1, k/2)
+               , rep(0, k/2))
+
+beta_true <- rep(beta_true, 3)
+
+# Calculate squared bias
+squared_bias <- (means - beta_true)^2
+
+# Add column names to squared_bias
+colnames(squared_bias) <- colnames(means)
+
+# Display squared_bias
+squared_bias_cc <- sum(squared_bias[1:as.numeric(k+1)])
+squared_bias_cc
+squared_bias_wcc <- sum(squared_bias[as.numeric(k+2):as.numeric(k+k+2)])
+squared_bias_wcc
+squared_bias_lcc <- sum(squared_bias[as.numeric(k+k+3):length(squared_bias)])
+squared_bias_lcc
+
+# Take the variance of the realizations
+variances <- apply(res, 2, var)
+
+var_cc <- sum(variances[1:as.numeric(k+1)])
+var_cc
+var_wcc <- sum(variances[as.numeric(k+2):as.numeric(k+k+2)])
+var_wcc
+var_lcc <- sum(variances[as.numeric(k+k+3):length(variances)])
+var_lcc
+
+write.csv(res, file = "~/Documents/Master/thesis/02-Thesis/code/code-MA-thesis/output/sim_e.csv", row.names = FALSE)
+
+
+# Simulation F
+
+set.seed(123)
+
+sim <- 300
+k <- 36
+N <- 100000
+r <- 0.9
+a <- 0.9
+ns_fixed1 <- 4000
+ns_fixed2 <- 2000
+
+mean1 <- c(rep(1, k/2), rep(0, k/2))
+mean0 <- c(rep(0, k))
+cov_mat <- diag(k)
+
+beta_names_cc <- paste0("β_hat_cc_", 0:k)
+beta_names_wcc <- paste0("β_hat_wcc_", 0:k)
+beta_names_lcc <- paste0("β_hat_lcc_", 0:k)
+
+
+output <- c(beta_names_cc, beta_names_wcc, beta_names_lcc)
+
+res <- data.frame(matrix(ncol = length(output), nrow = 0))
+colnames(res) <- output
+
+for (i in 1:sim) {
+  
+  df <- gdp.imbalanced(N = N, r = r, distribution= "gaussian", k = k, mean1 = mean1
+                       , mean0 = mean0, sigma1 = cov_mat, sigma0 = cov_mat)
+  
+  
+  cc_output <- cc_algorithm_fixed(data=df, r=r, a=a, ns_fixed=ns_fixed1)
+  coef_adjusted_cc <- cc_output$coef_adjusted
+  
+  wcc_output <- wcc_algorithm_fixed(data=df, r=r, a=a, ns_fixed = ns_fixed1)
+  coef_unadjusted_wcc <- wcc_output$coef_unadjusted
+  
+  lcc_output <- lcc_algorithm_fixed(data=df, r=r, a_wcc=a, ns_fixed = ns_fixed2)
+  coef_adjusted_lcc <- lcc_output$coef_adjusted
+  
+  # a_bar(lambda)
+  #a_bar_lcc <- mean(lcc_output$a_bar_lcc)
+  
+  # Predicting subsampling
+  # y_hat_cc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_cc)
+  # y_hat_wcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_unadjusted_wcc)
+  # y_hat_lcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_lcc)
+  
+  # Coefficients
+  res_cc <- data.frame(t(coef_adjusted_cc))
+  colnames(res_cc) <- beta_names_cc
+  
+  res_wcc <- data.frame(t(coef_unadjusted_wcc))
+  colnames(res_wcc) <- beta_names_wcc
+  
+  res_lcc <- data.frame(t(coef_adjusted_lcc))
+  colnames(res_lcc) <- beta_names_lcc
+  
+  #AUC
+  
+  # auc_cc <- as.numeric(roc(df_test$y, y_hat_cc)$auc)
+  # auc_wcc <- as.numeric(roc(df_test$y, y_hat_wcc)$auc)
+  # auc_lcc <- as.numeric(roc(df_test$y, y_hat_lcc)$auc)
+  
+  res <- rbind(res
+               , cbind(res_cc, res_wcc, res_lcc))
+  
+}
+
+# take the mean of the results
+means <- data.frame(t(colMeans(res)))
+colnames(means) <- gsub("β_hat_", "", colnames(means))
+
+
+# True coefficient values
+beta_true <- c(get.true.intercept(1-r, rep(0.5, k), c(rep(1,k/2), rep(0, k/2))), rep(1, k/2)
+               , rep(0, k/2))
+
+beta_true <- rep(beta_true, 3)
+
+# Calculate squared bias
+squared_bias <- (means - beta_true)^2
+
+# Add column names to squared_bias
+colnames(squared_bias) <- colnames(means)
+
+# Display squared_bias
+squared_bias_cc <- sum(squared_bias[1:as.numeric(k+1)])
+squared_bias_cc
+squared_bias_wcc <- sum(squared_bias[as.numeric(k+2):as.numeric(k+k+2)])
+squared_bias_wcc
+squared_bias_lcc <- sum(squared_bias[as.numeric(k+k+3):length(squared_bias)])
+squared_bias_lcc
+
+# Take the variance of the realizations
+variances <- apply(res, 2, var)
+
+var_cc <- sum(variances[1:as.numeric(k+1)])
+var_cc
+var_wcc <- sum(variances[as.numeric(k+2):as.numeric(k+k+2)])
+var_wcc
+var_lcc <- sum(variances[as.numeric(k+k+3):length(variances)])
+var_lcc
+
+write.csv(res, file = "~/Documents/Master/thesis/02-Thesis/code/code-MA-thesis/output/sim_f.csv", row.names = FALSE)
+
+
+
+# Simulation G
+
+set.seed(123)
+
+sim <- 300
+k <- 26
+N <- 100000
+r <- 0.9
+a <- 0.9
+ns_fixed1 <- 4000
+ns_fixed2 <- 2000
+
+mean1 <- c(rep(1, k/2), rep(0, k/2))
+mean0 <- c(rep(0, k))
+cov_mat <- diag(k)
+
+beta_names_cc <- paste0("β_hat_cc_", 0:k)
+beta_names_wcc <- paste0("β_hat_wcc_", 0:k)
+beta_names_lcc <- paste0("β_hat_lcc_", 0:k)
+
+
+output <- c(beta_names_cc, beta_names_wcc, beta_names_lcc)
+
+res <- data.frame(matrix(ncol = length(output), nrow = 0))
+colnames(res) <- output
+
+for (i in 1:sim) {
+  
+  df <- gdp.imbalanced(N = N, r = r, distribution= "gaussian", k = k, mean1 = mean1
+                       , mean0 = mean0, sigma1 = cov_mat, sigma0 = cov_mat)
+  
+  
+  cc_output <- cc_algorithm_fixed(data=df, r=r, a=a, ns_fixed=ns_fixed1)
+  coef_adjusted_cc <- cc_output$coef_adjusted
+  
+  wcc_output <- wcc_algorithm_fixed(data=df, r=r, a=a, ns_fixed = ns_fixed1)
+  coef_unadjusted_wcc <- wcc_output$coef_unadjusted
+  
+  lcc_output <- lcc_algorithm_fixed(data=df, r=r, a_wcc=a, ns_fixed = ns_fixed2)
+  coef_adjusted_lcc <- lcc_output$coef_adjusted
+  
+  # a_bar(lambda)
+  #a_bar_lcc <- mean(lcc_output$a_bar_lcc)
+  
+  # Predicting subsampling
+  # y_hat_cc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_cc)
+  # y_hat_wcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_unadjusted_wcc)
+  # y_hat_lcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_lcc)
+  
+  # Coefficients
+  res_cc <- data.frame(t(coef_adjusted_cc))
+  colnames(res_cc) <- beta_names_cc
+  
+  res_wcc <- data.frame(t(coef_unadjusted_wcc))
+  colnames(res_wcc) <- beta_names_wcc
+  
+  res_lcc <- data.frame(t(coef_adjusted_lcc))
+  colnames(res_lcc) <- beta_names_lcc
+  
+  #AUC
+  
+  # auc_cc <- as.numeric(roc(df_test$y, y_hat_cc)$auc)
+  # auc_wcc <- as.numeric(roc(df_test$y, y_hat_wcc)$auc)
+  # auc_lcc <- as.numeric(roc(df_test$y, y_hat_lcc)$auc)
+  
+  res <- rbind(res
+               , cbind(res_cc, res_wcc, res_lcc))
+  
+}
+
+# take the mean of the results
+means <- data.frame(t(colMeans(res)))
+colnames(means) <- gsub("β_hat_", "", colnames(means))
+
+
+# True coefficient values
+beta_true <- c(get.true.intercept(1-r, rep(0.5, k), c(rep(1,k/2), rep(0, k/2))), rep(1, k/2)
+               , rep(0, k/2))
+
+beta_true <- rep(beta_true, 3)
+
+# Calculate squared bias
+squared_bias <- (means - beta_true)^2
+
+# Add column names to squared_bias
+colnames(squared_bias) <- colnames(means)
+
+# Display squared_bias
+squared_bias_cc <- sum(squared_bias[1:as.numeric(k+1)])
+squared_bias_cc
+squared_bias_wcc <- sum(squared_bias[as.numeric(k+2):as.numeric(k+k+2)])
+squared_bias_wcc
+squared_bias_lcc <- sum(squared_bias[as.numeric(k+k+3):length(squared_bias)])
+squared_bias_lcc
+
+# Take the variance of the realizations
+variances <- apply(res, 2, var)
+
+var_cc <- sum(variances[1:as.numeric(k+1)])
+var_cc
+var_wcc <- sum(variances[as.numeric(k+2):as.numeric(k+k+2)])
+var_wcc
+var_lcc <- sum(variances[as.numeric(k+k+3):length(variances)])
+var_lcc
+
+write.csv(res, file = "~/Documents/Master/thesis/02-Thesis/code/code-MA-thesis/output/sim_g.csv", row.names = FALSE)
+
+
+
+# Simulation H, 25 min
+
+set.seed(123)
+
+sim <- 1000
+k <- 26
+N <- 100000
+r <- 0.9
+a <- 0.9
+ns_fixed1 <- 4000
+ns_fixed2 <- 2000
+
+mean1 <- c(rep(1, k/2), rep(0, k/2))
+mean0 <- c(rep(0, k))
+cov_mat <- diag(k)
+
+beta_names_cc <- paste0("β_hat_cc_", 0:k)
+beta_names_wcc <- paste0("β_hat_wcc_", 0:k)
+beta_names_lcc <- paste0("β_hat_lcc_", 0:k)
+
+
+output <- c(beta_names_cc, beta_names_wcc, beta_names_lcc)
+
+res <- data.frame(matrix(ncol = length(output), nrow = 0))
+colnames(res) <- output
+
+for (i in 1:sim) {
+  
+  df <- gdp.imbalanced(N = N, r = r, distribution= "gaussian", k = k, mean1 = mean1
+                       , mean0 = mean0, sigma1 = cov_mat, sigma0 = cov_mat)
+  
+  
+  cc_output <- cc_algorithm_fixed(data=df, r=r, a=a, ns_fixed=ns_fixed1)
+  coef_adjusted_cc <- cc_output$coef_adjusted
+  
+  wcc_output <- wcc_algorithm_fixed(data=df, r=r, a=a, ns_fixed = ns_fixed1)
+  coef_unadjusted_wcc <- wcc_output$coef_unadjusted
+  
+  lcc_output <- lcc_algorithm_fixed(data=df, r=r, a_wcc=a, ns_fixed = ns_fixed2)
+  coef_adjusted_lcc <- lcc_output$coef_adjusted
+  
+  # a_bar(lambda)
+  #a_bar_lcc <- mean(lcc_output$a_bar_lcc)
+  
+  # Predicting subsampling
+  # y_hat_cc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_cc)
+  # y_hat_wcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_unadjusted_wcc)
+  # y_hat_lcc <- logit_predict(df_test, c(paste0("X", 1:k)), coef_adjusted_lcc)
+  
+  # Coefficients
+  res_cc <- data.frame(t(coef_adjusted_cc))
+  colnames(res_cc) <- beta_names_cc
+  
+  res_wcc <- data.frame(t(coef_unadjusted_wcc))
+  colnames(res_wcc) <- beta_names_wcc
+  
+  res_lcc <- data.frame(t(coef_adjusted_lcc))
+  colnames(res_lcc) <- beta_names_lcc
+  
+  #AUC
+  
+  # auc_cc <- as.numeric(roc(df_test$y, y_hat_cc)$auc)
+  # auc_wcc <- as.numeric(roc(df_test$y, y_hat_wcc)$auc)
+  # auc_lcc <- as.numeric(roc(df_test$y, y_hat_lcc)$auc)
+  
+  res <- rbind(res
+               , cbind(res_cc, res_wcc, res_lcc))
+  
+}
+
+# take the mean of the results
+means <- data.frame(t(colMeans(res)))
+colnames(means) <- gsub("β_hat_", "", colnames(means))
+
+
+# True coefficient values
+beta_true <- c(get.true.intercept(1-r, rep(0.5, k), c(rep(1,k/2), rep(0, k/2))), rep(1, k/2)
+               , rep(0, k/2))
+
+beta_true <- rep(beta_true, 3)
+
+# Calculate squared bias
+squared_bias <- (means - beta_true)^2
+
+# Add column names to squared_bias
+colnames(squared_bias) <- colnames(means)
+
+# Display squared_bias
+squared_bias_cc <- sum(squared_bias[1:as.numeric(k+1)])
+squared_bias_cc
+squared_bias_wcc <- sum(squared_bias[as.numeric(k+2):as.numeric(k+k+2)])
+squared_bias_wcc
+squared_bias_lcc <- sum(squared_bias[as.numeric(k+k+3):length(squared_bias)])
+squared_bias_lcc
+
+# Take the variance of the realizations
+variances <- apply(res, 2, var)
+
+var_cc <- sum(variances[1:as.numeric(k+1)])
+var_cc
+var_wcc <- sum(variances[as.numeric(k+2):as.numeric(k+k+2)])
+var_wcc
+var_lcc <- sum(variances[as.numeric(k+k+3):length(variances)])
+var_lcc
+
+write.csv(res, file = "~/Documents/Master/thesis/02-Thesis/code/code-MA-thesis/output/sim_h.csv", row.names = FALSE)
+
+
+
+
+
 
