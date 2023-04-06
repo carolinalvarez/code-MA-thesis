492b4ec74efe23987cf064821a81008524a885c9
carolinalvarez <carolina.alvarezg21@gmail.com>
2023-04-06 23:50:14 +0200

    added more sim to sim3

--- R/sim_3.R
+++ R/sim_3.R
@@ -161,7 +161,7 @@ var_logit <- sum(variances[as.numeric(k+k+k+5):length(squared_bias)-1])
 var_logit
 
 
-write.csv(res, file = paste0(path_output, "sim_P_a"), row.names = TRUE)
+write.csv(res, file = paste0(path_output, "sim_Prob_a"), row.names = TRUE)
 
 
 
@@ -639,3 +639,199 @@ var_logit
 
 
 write.csv(res, file = paste0(path_output, "sim_Prob_e"), row.names = TRUE)
+
+
+####################################################################################
+#################### Repeat simulations but with a smaller k #######################
+#################################################################################### 
+
+path_output <-"~/Documents/Master/thesis/02-Thesis/code/code-MA-thesis/output/"
+# I need to recalculate the subsample sizes...
+
+# fixed parameters
+sim=100
+N = 10^5
+k = 10
+mean1 <- c(rep(1, k/2), rep(0, k/2))
+mean0 <- c(rep(0, k))
+cov_mat <- diag(k)
+
+# getting average subsample LCC for each r value
+
+r_values <- c(0.7, 0.8, 0.9, 0.95, 0.99)
+
+results <- list()
+
+set.seed(123)
+
+for (r in r_values) {
+  result <- average_subsample_size(N=N, k=k, a=r, r=r, mean1=mean1, mean0=mean0
+                                   , sigma1=cov_mat, sigma0=cov_mat, sim=sim)
+  results[[as.character(r)]] <- result
+}
+
+df_subsamples <- do.call(rbind, results)
+write.csv(df_subsamples, file = paste0(path_output, "sim3_average_subsamples_LCC_2"),
+          row.names = TRUE)
+
+
+
+
+# General parameters
+k = 10
+sim <- 1000
+N <- 10^5
+#####################
+
+# sim Prob a
+r_a <- 0.7
+a_a <- 0.7
+ns_fixed1_a <- 33400
+ns_fixed2_a <- 16700
+
+set.seed(123)
+res_a <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_a, a=a_a, ns_fixed1 = ns_fixed1_a,
+                                  ns_fixed2 = ns_fixed2_a, path_output = path_output, 
+                                  name_res = "sim_Prob_a_2")
+#res_analysis_a <- res_analysis_sim3(res = res_a, k=k, a = a_a)
+
+
+# sim prob b
+r_b <- 0.8
+a_b <- 0.8
+ns_fixed1_b <- 27600
+ns_fixed2_b <- 13800
+
+set.seed(123)
+res_b <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_b, a=a_b, ns_fixed1 = ns_fixed1_b,
+                                   ns_fixed2 = ns_fixed2_b, path_output = path_output, 
+                                   name_res = "sim_Prob_b_2")
+
+# sim prob c
+r_c <- 0.9
+a_c <- 0.9
+ns_fixed1_c <- 18000
+ns_fixed2_c <- 9000
+
+set.seed(123)
+res_c <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_c, a=a_c, ns_fixed1 = ns_fixed1_c,
+                                   ns_fixed2 = ns_fixed2_c, path_output = path_output, 
+                                   name_res = "sim_Prob_c_2")
+
+
+# sim prob d
+r_d <- 0.95
+a_d <- 0.95
+ns_fixed1_d <- 11200
+ns_fixed2_d <- 5600
+
+set.seed(123)
+res_d <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_d, a=a_d, ns_fixed1 = ns_fixed1_d,
+                                   ns_fixed2 = ns_fixed2_d, path_output = path_output, 
+                                   name_res = "sim_Prob_d_2")
+
+# sim prob e
+r_e <- 0.99
+a_e <- 0.99
+ns_fixed1_e <- 3100
+ns_fixed2_e <- 1550
+
+set.seed(123)
+res_e <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_e, a=a_e, ns_fixed1 = ns_fixed1_e,
+                                   ns_fixed2 = ns_fixed2_e, path_output = path_output, 
+                                   name_res = "sim_Prob_e_2")
+
+################################################################################
+
+# fixed parameters
+sim=100
+N = 10^5
+k = 20
+mean1 <- c(rep(1, k/2), rep(0, k/2))
+mean0 <- c(rep(0, k))
+cov_mat <- diag(k)
+
+# getting average subsample LCC for each r value
+
+r_values <- c(0.7, 0.8, 0.9, 0.95, 0.99)
+
+results <- list()
+
+set.seed(123)
+
+for (r in r_values) {
+  result <- average_subsample_size(N=N, k=k, a=r, r=r, mean1=mean1, mean0=mean0
+                                   , sigma1=cov_mat, sigma0=cov_mat, sim=sim)
+  results[[as.character(r)]] <- result
+}
+
+df_subsamples <- do.call(rbind, results)
+write.csv(df_subsamples, file = paste0(path_output, "sim3_average_subsamples_LCC_3"),
+          row.names = TRUE)
+
+
+
+
+# General parameters
+k = 20
+sim <- 1000
+N <- 10^5
+#####################
+
+# sim Prob a
+r_a <- 0.7
+a_a <- 0.7
+ns_fixed1_a <- 15000
+ns_fixed2_a <- 7500
+
+set.seed(123)
+res_a <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_a, a=a_a, ns_fixed1 = ns_fixed1_a,
+                                   ns_fixed2 = ns_fixed2_a, path_output = path_output, 
+                                   name_res = "sim_Prob_a_3")
+#res_analysis_a <- res_analysis_sim3(res = res_a, k=k, a = a_a)
+
+
+# sim prob b
+r_b <- 0.8
+a_b <- 0.8
+ns_fixed1_b <- 12600
+ns_fixed2_b <- 6300
+
+set.seed(123)
+res_b <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_b, a=a_b, ns_fixed1 = ns_fixed1_b,
+                                   ns_fixed2 = ns_fixed2_b, path_output = path_output, 
+                                   name_res = "sim_Prob_b_3")
+
+# sim prob c
+r_c <- 0.9
+a_c <- 0.9
+ns_fixed1_c <- 8800
+ns_fixed2_c <- 4400
+
+set.seed(123)
+res_c <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_c, a=a_c, ns_fixed1 = ns_fixed1_c,
+                                   ns_fixed2 = ns_fixed2_c, path_output = path_output, 
+                                   name_res = "sim_Prob_c_3")
+
+
+# sim prob d
+r_d <- 0.95
+a_d <- 0.95
+ns_fixed1_d <- 5600
+ns_fixed2_d <- 2800
+
+set.seed(123)
+res_d <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_d, a=a_d, ns_fixed1 = ns_fixed1_d,
+                                   ns_fixed2 = ns_fixed2_d, path_output = path_output, 
+                                   name_res = "sim_Prob_d_3")
+
+# sim prob e
+r_e <- 0.99
+a_e <- 0.99
+ns_fixed1_e <- 2000
+ns_fixed2_e <- 1000
+
+set.seed(123)
+res_e <- monte_carlo_runnings_sim_3_4(sim=sim, k=k, N=N, r=r_e, a=a_e, ns_fixed1 = ns_fixed1_e,
+                                   ns_fixed2 = ns_fixed2_e, path_output = path_output, 
+                                   name_res = "sim_Prob_e_3")
